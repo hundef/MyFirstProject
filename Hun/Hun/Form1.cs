8e41ead6139e6882c82c9262a076377130f683e6
hundef <hundef@gmail.com>
2017-02-03 23:02:46 -0800

    hahaha

--- Hun/Hun/Form1.cs
+++ Hun/Hun/Form1.cs
@@ -20,6 +20,7 @@ namespace Hun
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Coming soon!");
+            MessageBox.Show("am reached!");
         }
     }
 }
